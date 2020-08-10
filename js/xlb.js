c2776b3fee7a05cb60b33b83c997cdbeab47236c
Lollipop <moluuser@gmail.com>
2020-08-10 13:30:17 +0800

    add

--- js/xlb.js
+++ js/xlb.js
@@ -2,7 +2,7 @@ ca.init();
 var to_order = ca.id('to_order');
 to_order.addEventListener('tap', function(){
 	ca.newInterface({
-		url:'order.html',
-		id:'order'
+		url:'go.html',
+		id:'go'
 	});
 });
